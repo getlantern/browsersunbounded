40ed438b70b1f4efbb6e582ffd490597f361cf99
Ox Cart <ox.to.a.cart@gmail.com>
2022-12-06 18:09:48 -0600

    Demonstrate failure when two clients are hitting the same desktop proxy and one is a slow reader

--- useragent/useragent.go
+++ useragent/useragent.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -13,14 +14,14 @@ const (
 )
 
 func main() {
-	client1 := http.Client{
+	client1 := &http.Client{
 		Transport: &http.Transport{
 			Proxy: func(*http.Request) (*url.URL, error) {
 				return url.Parse("http://127.0.0.1:1080")
 			},
 		},
 	}
-	client2 := http.Client{
+	client2 := &http.Client{
 		Transport: &http.Transport{
 			Proxy: func(*http.Request) (*url.URL, error) {
 				return url.Parse("http://127.0.0.1:1081")
@@ -29,31 +30,45 @@ func main() {
 	}
 
 	var wg sync.WaitGroup
-	wg.Add(2)
 
-	// Download something large on a couple of goroutines, one of which misbehaves and doesn't read the response
-	go func() {
-		defer wg.Done()
+	launchGoodClient := func(port string, client *http.Client, indicator string) {
+		wg.Add(1)
 
-		resp, err := client1.Get(testFile)
-		if err != nil {
-			log.Fatalf("Client1 request error %v\n", err)
-		}
-		total := 0
-		b := make([]byte, 8192)
-		for {
-			n, err := resp.Body.Read(b)
-			total += n
-			if err == io.EOF {
-				log.Printf("Client1 read %d bytes\n", total)
-				return
-			}
+		log.Printf("Launching good client to %v\n", port)
+
+		go func() {
+			defer wg.Done()
+
+			resp, err := client.Get(testFile)
 			if err != nil {
-				log.Fatalf("Client1 read error %v\n", err)
+				log.Fatalf("Good Client to %v request error %v\n", port, err)
 			}
-		}
-	}()
+			total := 0
+			increment := 0
+			b := make([]byte, 8192)
+			for {
+				n, err := resp.Body.Read(b)
+				total += n
+				increment += n
+				if err == io.EOF {
+					log.Printf("Good Client to %v read %d bytes\n", port, total)
+					return
+				}
+				if err != nil {
+					log.Fatalf("Good Client to %v Client1 read error %v\n", port, err)
+				}
+				if increment > 1024768 {
+					fmt.Print(indicator)
+					increment = 0
+				}
+			}
+		}()
+	}
 
+	launchGoodClient("1080", client1, ".")
+	launchGoodClient("1081", client2, "x")
+
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
@@ -62,7 +77,6 @@ func main() {
 			log.Fatalf("Client1 request error %v\n", err)
 		}
 		// ignore response body
-		return
 	}()
 
 	wg.Wait()
