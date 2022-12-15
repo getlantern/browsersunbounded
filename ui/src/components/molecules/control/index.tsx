15dde0f0a2fd48fe839828f3ed5e68f39538e621
woodybury <whshortridge@gmail.com>
2022-12-15 10:18:05 -0500

    rm connection type fallback re ox good points

--- ui/src/components/molecules/control/index.tsx
+++ ui/src/components/molecules/control/index.tsx
@@ -4,9 +4,6 @@ import {useEmitterState} from '../../../hooks/useStateEmitter'
 import {readyEmitter, sharingEmitter, wasmInterface} from '../../../utils/wasmInterface'
 import Info from '../info'
 import {TextInfo} from './styles'
-import {ConnectionTypes, useConnectionType} from '../../../hooks/useConnectionType'
-import {useEffect} from 'react'
-import {isMobile} from '../../../utils/isMobile'
 
 interface Props {
 	onToggle?: (s: boolean) => void
@@ -16,13 +13,6 @@ interface Props {
 const Control = ({onToggle, info = false}: Props) => {
 	const ready = useEmitterState(readyEmitter)
 	const sharing = useEmitterState(sharingEmitter)
-	const connectionType = useConnectionType(sharing) // subscribe to connection type for users in sharing state
-
-	useEffect(() => {
-		if (isMobile || connectionType === ConnectionTypes.SLOW) {
-			// @todo design for connection/data warnings
-		}
-	}, [connectionType])
 
 	const _onToggle = (share: boolean) => {
 		if (share) wasmInterface.start()
