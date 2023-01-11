db50a2c495153d383edbc7534d8529e7cc905ad7
Matt Joiner <anacrolix@gmail.com>
2023-01-11 11:43:05 +1100

    Fix shebangs
    MacOS disagreed about where bash is, and I think this is more portable.

--- client/build.sh
+++ client/build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 set -x
 cd go
 go build -race -o ../dist/bin/$1 --ldflags="-X 'main.clientType=$1'"
--- client/build_web.sh
+++ client/build_web.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 set -x
 cd go
 GOOS=js GOARCH=wasm go build --ldflags="-X 'main.clientType=widget'" -o ../dist/public/widget.wasm
