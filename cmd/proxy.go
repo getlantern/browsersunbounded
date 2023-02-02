f737906f9e50c1a90f4d235efa1b2f14b8d88d01
William Forkner <1120829+forkner@users.noreply.github.com>
2023-02-02 12:54:34 -0500

    fix typos

--- cmd/proxy.go
+++ cmd/proxy.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"context"
-	"crypto/tls"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
-	"net/url"
-	"sync"
 	"time"
 
 	"github.com/elazarl/goproxy"
 	"github.com/getlantern/broflake/clientcore"
-	"github.com/getlantern/broflake/common"
-	"github.com/lucas-clemente/quic-go"
 )
 
 const (
@@ -25,19 +18,17 @@ func runLocalProxy(port string, bfconn *clientcore.BroflakeConn) {
 	// TODO: this is just to prevent a race with client boot processes, it's not worth getting too
 	// fancy with an event-driven solution because the local proxy is all mocked functionality anyway
 	<-time.After(2 * time.Second)
-
-	tlsConf := &tls.Config{
-		InsecureSkipVerify: true,
-		NextProtos:         []string{"broflake"},
-	}
-
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
 	// This tells goproxy to wrap the dial function in a chained CONNECT request
 	proxy.ConnectDial = proxy.NewConnectDialToProxy("http://i.do.nothing")
 
-	ql := clientcore.NewQUICLayer(bfconn, &clientcore.QUICOptions{InsecureSkipVerify: true})
-	proxy.Tr = CreateHTTPTransport(ql)
+	ql, err := clientcore.NewQUICLayer(bfconn, &clientcore.QUICOptions{InsecureSkipVerify: true})
+	if err != nil {
+		log.Printf("Cannot start local HTTP proxy: failed to create QUIC layer: %v", err)
+		return
+	}
+	proxy.Tr = clientcore.CreateHTTPTransport(ql)
 
 	proxy.OnRequest().DoFunc(
 		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
