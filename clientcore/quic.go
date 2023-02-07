6e61949f32ce3a93031f852bc77b0b452818d1b6
William Forkner <1120829+forkner@users.noreply.github.com>
2023-02-07 10:06:19 -0500

    use DialContext for http.Transport

--- clientcore/quic.go
+++ clientcore/quic.go
@@ -22,7 +22,7 @@ func CreateHTTPTransport(c ReliableStreamLayer) *http.Transport {
 		Proxy: func(req *http.Request) (*url.URL, error) {
 			return url.Parse("http://i.do.nothing")
 		},
-		Dial: func(network, addr string) (net.Conn, error) {
+		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
 			return c.DialContext(context.Background())
 		},
 	}
