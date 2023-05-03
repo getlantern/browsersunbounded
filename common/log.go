6f2955e1e104a7ce773d71252be85a15cf30f446
Ox Cart <ox.to.a.cart@gmail.com>
2023-05-03 06:18:33 -0500

    Added common/log.go

---
+++ common/log.go
@@ -0,0 +1,35 @@
+package common
+
+import (
+	"log"
+	"os"
+	"sync"
+)
+
+var (
+	debugLogger = log.New(os.Stderr, "", log.LstdFlags)
+	logMx       sync.RWMutex
+)
+
+// Override the Logger used by broflake for debug messages
+func SetDebugLogger(l *log.Logger) {
+	logMx.Lock()
+	defer logMx.Unlock()
+	debugLogger = l
+}
+
+// Log a debug level message
+func Debugf(format string, args ...interface{}) {
+	logMx.RLock()
+	l := debugLogger
+	logMx.RUnlock()
+	l.Printf(format+"\n", args...)
+}
+
+// Log a debug level message
+func Debug(msg interface{}) {
+	logMx.RLock()
+	l := debugLogger
+	logMx.RUnlock()
+	l.Println(msg)
+}
