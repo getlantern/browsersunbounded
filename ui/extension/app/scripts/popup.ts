043b9d14d00ee3ef3e45f9b2244e5acd8f200286
woodybury <whshortridge@gmail.com>
2023-09-25 17:12:46 -0400

    fix new logo width on extensions

--- ui/extension/app/scripts/popup.ts
+++ ui/extension/app/scripts/popup.ts
@@ -14,7 +14,7 @@ const popupApp = async () => {
 	// set iframe styles based on user theme
 	iframe.setAttribute(
 		'style',
-		`background-color: ${backgroundColor}; width: 334px; height: 588px; border: none; margin: 0; padding: 0;` // 4px buffer seems needed to hide unnecessary scrollbars
+		`background-color: ${backgroundColor}; width: 330px; height: 588px; border: none; margin: 0; padding: 0;` // 4px buffer seems needed to hide unnecessary scrollbars
 	)
 	document.body.appendChild(iframe)
 	await bindPopup(iframe)
