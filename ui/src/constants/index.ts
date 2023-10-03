dca5f8ad623dd247ce105e12333e4ed2d295868c
woodybury <whshortridge@gmail.com>
2023-10-03 16:54:28 -0400

    sub out extension stores

--- ui/src/constants/index.ts
+++ ui/src/constants/index.ts
@@ -121,7 +121,10 @@ export const WASM_CLIENT_CONFIG = {
 	egressEndpoint: process.env.REACT_APP_EGRESS_ENDPOINT!
 }
 
+// @todo rm stubbing out store urls until extension is ready
 export const APP_STORE_LINKS = {
-	chrome: 'https://chrome.google.com/webstore/detail/browsers-unbounded/jonhnkjdlimggpmbehgkgpjgphoepfdj/',
-	firefox: 'https://addons.mozilla.org/en-US/firefox/addon/browsers-unbounded/'
+	// chrome: 'https://chrome.google.com/webstore/detail/browsers-unbounded/jonhnkjdlimggpmbehgkgpjgphoepfdj',
+	chrome: 'https://unbounded.lantern.io/coming-soon',
+	// firefox: 'https://addons.mozilla.org/en-US/firefox/addon/browsers-unbounded/',
+	firefox: 'https://unbounded.lantern.io/coming-soon',
 }
