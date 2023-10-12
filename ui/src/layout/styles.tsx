48bdf174debc5100d71dd9fd65aa358cc34fe167
woodybury <whshortridge@gmail.com>
2023-10-12 15:16:47 -0400

    Minor BU UI issues #449

--- ui/src/layout/styles.tsx
+++ ui/src/layout/styles.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import {COLORS, Layouts, Themes} from '../constants'
+import {COLORS, Layouts, SHADOWS, Themes} from '../constants'
 
 interface Props {
   theme: Themes
@@ -12,13 +12,13 @@ const AppWrapper = styled.section`
   display: flex;
   width: 100%;
   max-width: ${({layout}: Props) => layout === Layouts.PANEL ? '330px' : layout === Layouts.FLOATING ? '360px'  : 'unset'};
-  border-radius: ${({layout}: Props) => layout !== Layouts.BANNER ? layout === Layouts.FLOATING ? '32px 23px 0 0' : '32px' : '0'};
+  border-radius: ${({layout}: Props) => layout !== Layouts.BANNER ? layout === Layouts.FLOATING ? '32px 32px 0 0' : '32px' : '0'};
   background-color: ${({theme, $menu}: Props) => theme === Themes.DARK ? COLORS.grey5 : $menu ? COLORS.grey1 : COLORS.white };
   box-sizing: content-box;
   position: ${({layout}: Props) => layout !== Layouts.FLOATING ? 'relative' : 'fixed'};
   ${({layout}: Props) => layout === Layouts.FLOATING ? 'bottom: 0;\n  right: 10px;\n z-index: 2147483646;' : ''} // 2147483647 is largest positive value of a signed integer on a 32 bit system
   margin: 0 auto;
-
+  box-shadow: ${({layout}: Props) => layout === Layouts.FLOATING ? SHADOWS.light : ''};
   * {
     box-sizing: unset;
   }
