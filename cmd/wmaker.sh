44efd94685978d87d3ecc330efb470b7b8b9db31
Noah Levenson <noahlevenson@gmail.com>
2023-11-20 12:44:11 -0800

    don't commit wmaker server IP addr

--- cmd/wmaker.sh
+++ cmd/wmaker.sh
@@ -5,7 +5,7 @@ set -e
 set -u
 
 # Details for the VM where we're hosting builds
-REMOTE_IP="144.126.209.50"
+REMOTE_IP="666.666.666.666"
 REMOTE_PORT="22"
 REMOTE_USERNAME="root"
 REMOTE_BASE_DIR="/usr/share/nginx/html"
