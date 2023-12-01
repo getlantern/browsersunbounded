9d625bb70f7fdcc79892aa8c0621e745f2fa02d1
woodybury <whshortridge@gmail.com>
2023-11-30 21:05:10 -0500

    Toggle lands below the fold in viewports 735 px and under #192

--- ui/src/components/organisms/banner/styles.tsx
+++ ui/src/components/organisms/banner/styles.tsx
@@ -40,7 +40,7 @@ export const BodyWrapper = styled.div`
 export const Body = styled.div`
   display: flex;
   width: 100%;
-  gap: ${(props: {mobile: boolean}) => props.mobile ? '16px' : 0};
+  gap: ${(props: {mobile: boolean}) => props.mobile ? '8px' : 0};
   max-width: ${MAX_WIDTH}px;
   flex-direction: ${(props: {mobile: boolean}) => props.mobile ? 'column' : 'row'};
 `
